94c3e436fe7b2ec3948a7464e0ea2b435a612dbc
rpruazol <rpruazol@gmail.com>
2022-10-14 21:26:18 +0000

    header styling

---
+++ note-taking-app-frontend/src/DropdownUser.js
@@ -0,0 +1,28 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, NavLink } from "react-router-dom";
+import User from './User';
+
+
+function DropdownUser(props) {
+  console.log(props)
+  return (
+    <>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          {props.user.given_name}
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <NavLink to="user-profile">
+            <h2>Profile</h2>
+          </NavLink>
+        </Dropdown.Menu>
+      </Dropdown>
+      <Routes>
+        <Route path="user-profile" element={<User />} />
+      </Routes>
+    </>
+  );
+}
+
+export default DropdownUser;
--- note-taking-app-frontend/src/User.js
+++ note-taking-app-frontend/src/User.js
@@ -5,16 +5,16 @@ const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return <div>Loading ....</div>;
   }
 
   return (
     isAuthenticated && (
-      <div>
+      <>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
-      </div>
+      </>
     )
   );
 };
